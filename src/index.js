2e4e6d40edd9e5786d8f923bdb28576a51bbb09e
ctokoli <matumasie@gamil.com>
2023-06-14 12:17:32 +0000

    configure webpack with html loader

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const todoList = {
+    "index": "1",
+    "title": "Booking a flight",
+    "description": "Going to book a flight",
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,33 @@
-module.export
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    devServer: {
+    static: {
+        directory: path.resolve(__dirname, 'dist')
+    },
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+    },
+    module: {
+        rules:[{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            title: 'Todo List',
+            filename: 'index.html',
+            template: 'src/index.html'
+        })
+    ]
+}
