99112fb80c2914915cdfe76809a8c168ccd6c5dc
ctokoli <matumasie@gamil.com>
2023-06-15 10:21:12 +0000

    made chnages after reviw

--- src/index.js
+++ src/index.js
@@ -2,32 +2,39 @@ import './styles/style.css';
 
 const todoList = [
   {
-    index: '1',
+    index: 2,
     description: 'Booking my flight tomorrow',
     completed: false,
   },
   {
-    index: '2',
+    index: 1,
     description: 'Visiting my children school friday',
     completed: false,
   },
   {
-    index: '3',
+    index: 3,
     description: 'Going for groceries shopping',
     completed: false,
   },
   {
-    index: '3',
+    index: 5,
     description: 'Visiting the gym',
+    completed: false,
+  },
+  {
+    index: 4,
+    description: 'Visiting the the market',
     completed: false,
   },
 ];
 
+
 const showTodo = () => {
   const listContainer = document.querySelector('.list-items');
   let placeholder = '';
-
-  todoList.forEach((todo) => {
+  let sortTodoList = todoList.sort(
+    (index1, index2) => (index1.index > index2.index) ? 1 : (index1.index < index2.index) ? -1 : 0);
+    sortTodoList.forEach((todo) => {
     placeholder += `
         <li>
             <div>
