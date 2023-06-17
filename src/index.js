d223cea13a853f9cf59901ccddfbdd7167da060b
ctokoli <matumasie@gamil.com>
2023-06-17 02:40:54 +0000

    reassign index in delete function

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ const deleteItem = () => {
     element.addEventListener('click', () => {
       const getIndex = element.parentElement.getAttribute('data-index');
       todoList = todoList.filter((todo) => todo.index !== parseInt(getIndex, 10));
+      todoList.forEach((value, indd) => {
+        value.index = indd + 1;
+      });
       localStorage.setItem('formdata', JSON.stringify(todoList));
       window.location.reload();
     });
