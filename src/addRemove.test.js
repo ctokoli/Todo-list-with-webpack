5cd02c0fcfa8709682949bb22439a965e11a9432
ctokoli <matumasie@gamil.com>
2023-06-21 15:31:54 +0000

    testing

---
+++ src/addRemove.test.js
@@ -0,0 +1,10 @@
+import addItem from './modules/addItems.js';
+import deleteItem from './modules/deleteItem.js';
+import localStorageMock from './setUpMock.js';
+
+describe ('add item to localstorage', () => {
+    test('should add json object to localstorage', () => { 
+       expect(); 
+     })
+})
+
