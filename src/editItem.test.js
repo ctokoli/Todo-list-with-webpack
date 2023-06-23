5ef67b17456f413b0e92278685747df8467d5a93
tasnimchy96 <tasnim.achy96@gmail.com>
2023-06-24 00:25:19 +0600

    Fix eslinter error

--- src/editItem.test.js
+++ src/editItem.test.js
@@ -18,68 +18,65 @@ describe('editing the task description', () => {
 
   it('Should update the description of the given index and update localStirage ', () => {
     const todoData = [
-        { index: 1, description: 'New Todo Item 1', completed: false },
-        { index: 2, description: 'New Todo Item 2', completed: false },
-        { index: 3, description: 'New Todo Item 3', completed: false },
+      { index: 1, description: 'New Todo Item 1', completed: false },
+      { index: 2, description: 'New Todo Item 2', completed: false },
+      { index: 3, description: 'New Todo Item 3', completed: false },
     ];
-    
+
     const index = 2;
     const value = 'New Updated Item';
 
     const updatedTodo = updateIndex(todoData, index, value);
 
     expect(updatedTodo).toEqual([
-        { index: 1, description: 'New Todo Item 1', completed: false },
-        { index: 2, description: 'New Updated Item', completed: false },
-        { index: 3, description: 'New Todo Item 3', completed: false },
-      ]);
-      expect(localStorage.setItem).toHaveBeenCalledWith(
-        "formdata",
-        JSON.stringify(updatedTodo),
-      );
-
+      { index: 1, description: 'New Todo Item 1', completed: false },
+      { index: 2, description: 'New Updated Item', completed: false },
+      { index: 3, description: 'New Todo Item 3', completed: false },
+    ]);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'formdata',
+      JSON.stringify(updatedTodo),
+    );
   });
 
   it('Should update completed of the given index and update localStirage ', () => {
     const todoData = [
-        { index: 1, description: 'New Todo Item 1', completed: false },
-        { index: 2, description: 'New Todo Item 2', completed: false },
-        { index: 3, description: 'New Todo Item 3', completed: false },
+      { index: 1, description: 'New Todo Item 1', completed: false },
+      { index: 2, description: 'New Todo Item 2', completed: false },
+      { index: 3, description: 'New Todo Item 3', completed: false },
     ];
-    
+
     const index = 3;
     const value = true;
 
     const updatedTodo = upDateCompleted(todoData, index, value);
 
     expect(updatedTodo).toEqual([
-        { index: 1, description: 'New Todo Item 1', completed: false },
-        { index: 2, description: 'New Todo Item 2', completed: false },
-        { index: 3, description: 'New Todo Item 3', completed: true },
-      ]);
-      expect(localStorage.setItem).toHaveBeenCalledWith(
-        "formdata",
-        JSON.stringify(updatedTodo),
-      );
-
+      { index: 1, description: 'New Todo Item 1', completed: false },
+      { index: 2, description: 'New Todo Item 2', completed: false },
+      { index: 3, description: 'New Todo Item 3', completed: true },
+    ]);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'formdata',
+      JSON.stringify(updatedTodo),
+    );
   });
   it('Should delete completed task if completed is true and update localStirage ', () => {
     const todoData = [
-        { index: 1, description: 'New Todo Item 1', completed: false },
-        { index: 2, description: 'New Todo Item 2', completed: true },
-        { index: 3, description: 'New Todo Item 3', completed: true },
+      { index: 1, description: 'New Todo Item 1', completed: false },
+      { index: 2, description: 'New Todo Item 2', completed: true },
+      { index: 3, description: 'New Todo Item 3', completed: true },
     ];
-    
+
     const bool = true;
     const updatedTodo = completedDelete(todoData, bool);
 
     expect(updatedTodo).toEqual([
-        { index: 1, description: 'New Todo Item 1', completed: false },
-      ]);
-      expect(localStorage.setItem).toHaveBeenCalledWith(
-        "formdata",
-        JSON.stringify(updatedTodo),
-      );
-
+      { index: 1, description: 'New Todo Item 1', completed: false },
+    ]);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'formdata',
+      JSON.stringify(updatedTodo),
+    );
   });
 });
--- src/modules/upDateCompleted.js
+++ src/modules/upDateCompleted.js
@@ -1,11 +1,11 @@
 const upDateCompleted = (sortTodoList, index, value) => {
-    sortTodoList.forEach((todo) => {
-      if (todo.index === parseInt(index, 10)) {
-        todo.completed = value;
-        localStorage.setItem('formdata', JSON.stringify(sortTodoList));
-      }
-    });
+  sortTodoList.forEach((todo) => {
+    if (todo.index === parseInt(index, 10)) {
+      todo.completed = value;
+      localStorage.setItem('formdata', JSON.stringify(sortTodoList));
+    }
+  });
   return sortTodoList;
-  };
+};
 
 export default upDateCompleted;
